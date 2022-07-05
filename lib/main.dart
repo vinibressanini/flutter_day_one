63d2057c6b0bb2edf04578e4fb4f0609467af8bd
Vinicius Marcus <vinimarcus41@gmail.com>
2022-07-05 14:35:21 -0300

    [feat] Added some widgets

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,13 @@ class HomePage extends StatelessWidget {
                 ),
               ],
               color: Colors.white,
+              gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Colors.grey.shade100,
+                    Colors.blue,
+                  ]),
               borderRadius: BorderRadius.circular(30),
             ),
             child: Column(
@@ -41,7 +48,7 @@ class HomePage extends StatelessWidget {
                 Padding(
                   padding: const EdgeInsets.only(left: 45, bottom: 6),
                   child: Text(
-                    "1 Feb 2019",
+                    "5 Jul 2022",
                     style: TextStyle(
                       fontSize: 16,
                       fontWeight: FontWeight.bold,
@@ -72,7 +79,7 @@ class HomePage extends StatelessWidget {
                   children: [
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
+                      children: [
                         Text(
                           "26 °C",
                           style: TextStyle(
@@ -83,13 +90,14 @@ class HomePage extends StatelessWidget {
                         SizedBox(height: 7),
                         Text(
                           "Indoor Temp",
-                          style: TextStyle(fontSize: 15, color: Colors.grey),
+                          style: TextStyle(
+                              fontSize: 15, color: Colors.grey.shade900),
                         )
                       ],
                     ),
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
+                      children: [
                         Text(
                           "48,2 %",
                           style: TextStyle(
@@ -100,13 +108,14 @@ class HomePage extends StatelessWidget {
                         SizedBox(height: 7),
                         Text(
                           "Outside Humidity",
-                          style: TextStyle(fontSize: 15, color: Colors.grey),
+                          style: TextStyle(
+                              fontSize: 15, color: Colors.grey.shade900),
                         )
                       ],
                     ),
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
+                      children: [
                         Text(
                           "57 %",
                           style: TextStyle(
@@ -117,7 +126,8 @@ class HomePage extends StatelessWidget {
                         SizedBox(height: 7),
                         Text(
                           "Indoor Humidity",
-                          style: TextStyle(fontSize: 15, color: Colors.grey),
+                          style: TextStyle(
+                              fontSize: 15, color: Colors.grey.shade900),
                         )
                       ],
                     ),
@@ -126,12 +136,11 @@ class HomePage extends StatelessWidget {
               ],
             ),
           ),
-          const SizedBox(height: 40),
+          const SizedBox(height: 20),
           SizedBox(
             height: 40,
             child: ListView(
               padding: const EdgeInsets.symmetric(horizontal: 15),
-
               scrollDirection: Axis.horizontal,
               children: const [
                 Padding(
@@ -194,39 +203,147 @@ class HomePage extends StatelessWidget {
                     ),
                   ),
                 ),
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text(
-                    "Living Room",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text(
-                    "Living Room",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text(
-                    "Living Room",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
               ],
             ),
-          )
+          ),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                width: 175,
+                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 30),
+                margin: const EdgeInsets.symmetric(horizontal: 10),
+                decoration: BoxDecoration(
+                  color: Colors.blue.shade700,
+                  borderRadius: BorderRadius.circular(30),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.blueGrey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 10,
+                      offset: Offset(4, 5),
+                    )
+                  ],
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: const [
+                        Icon(
+                          Icons.light,
+                          color: Colors.white,
+                          size: 32,
+                        ),
+                        Icon(
+                          Icons.circle,
+                          color: Colors.white,
+                          size: 25,
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 25,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "Lamp",
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          Text(
+                            "OPENED",
+                            style: TextStyle(
+                              fontSize: 17,
+                              color: Colors.grey.shade300,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                width: 175,
+                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 30),
+                margin: const EdgeInsets.symmetric(horizontal: 10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.blueGrey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 10,
+                      offset: Offset(4, 5),
+                    )
+                  ],
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children:  [
+                        Icon(
+                          Icons.router,
+                          color: Colors.blue.shade700,
+                          size: 32,
+                        ),
+                        Icon(
+                          Icons.circle,
+                          color: Colors.red.shade900,
+                          size: 25,
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 25,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "Router",
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          Text(
+                            "CLOSED",
+                            style: TextStyle(
+                              fontSize: 17,
+                              color: Colors.grey.shade600,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
